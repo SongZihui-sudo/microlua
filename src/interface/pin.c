d14f758d0def8dc9059e66de1e626d644b27f965
宋子慧 <1751122876@qq.com>
2023-07-14 18:03:27 +0800

    add: build for esp

--- src/interface/pin.c
+++ src/interface/pin.c
@@ -1,28 +1,6 @@
 #include "pin.h"
 #include <assert.h>
 
-void pin_table_init( pin_table* self )
-{
-    assert( self );
-    self->size = PIN_NUM;
-}
-
-void pin_table_value_index( pin_table* self, size_t index, bool value )
-{
-    assert( self );
-    if ( index < 0 || index > self->size )
-    {
-        printf( "index is out of the range!\n" );
-        return;
-    }
-    self->table[index]->value( self->table[index], value );
-}
-
-ABS_CTOR( pin_table )
-FUNCTION_SETTING( init, pin_table_init );
-FUNCTION_SETTING( item_value, pin_table_value_index )
-END_ABS_CTOR
-
 void pin_init( pin* self, int index )
 {
     assert( self );
--- src/interface/pin.h
+++ src/interface/pin.h
@@ -20,38 +20,14 @@ ABS_CLASS( pin )
 {
     int mIndex;
     bool mValue;
+    void ( *init )( pin * t, bool index );
 };
 
 INTERFACE( Ipin )
 {
-    void ( *init )( pin * t, bool index );
     void ( *value )( pin * t, bool value );
     void ( *on )( pin * t );
     void ( *off )( pin * t );
 };
-
-ABS_CLASS( pin_table )
-{
-    pin* table[PIN_NUM];
-    int size;
-    void ( *init )( pin_table* );
-    void ( *item_value )( pin_table, size_t, bool );
-};
-
-/**
- * @brief 初始化表
- *
- * @param self pin table 对象
- */
-void pin_table_init( pin_table* self );
-
-/**
- * @brief 设置指定下标的 pin
- *
- * @param self  pin table 对象
- * @param index  下标
- * @param value  值
- */
-void pin_table_value_index( pin_table* self, size_t index, bool value );
 
 #endif
